f8669eea2b0584fa6c14cd16fde646df5a2ccc15
Aditi0107 <aditi@tradifyservices.com>
2020-10-11 15:41:47 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { BookingComponent } from './booking/booking.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { MobileAppComponent } from './mobile-app/mobile-app.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    HeaderComponent,
+    AboutComponent,
+    ServicesComponent,
+    BookingComponent,
+    PricingComponent,
+    TestimonialsComponent,
+    MobileAppComponent,
+    BlogsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
