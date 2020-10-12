07d88274bcdf0e1bc49182bfb7846a4b2d84d142
Aditi0107 <aditi@tradifyservices.com>
2020-10-12 15:51:02 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BookingComponent } from './booking/booking.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home',component:HomeComponent},
+  { path:'about',component:AboutComponent},
+  { path:'services',component:ServicesComponent},
+  { path:'booking',component:BookingComponent},
+  { path:'pricing',component:PricingComponent},
+  { path:'testimonials',component:TestimonialsComponent},
+  { path:'blogs',component:BlogsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
