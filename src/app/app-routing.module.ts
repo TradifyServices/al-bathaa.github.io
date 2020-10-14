74cb0ac48538a229973803db180987f01a62d57c
Aditi0107 <aditi@tradifyservices.com>
2020-10-14 10:34:54 +0300

    adding sub pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BookingComponent } from './booking/booking.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',component:HomeComponent},
+  { path:'home',component:HomeComponent},
+  { path:'about',component:AboutComponent},
+  { path:'services',component:ServicesComponent},
+  { path:'booking',component:BookingComponent},
+  { path:'pricing',component:PricingComponent},
+  { path:'testimonials',component:TestimonialsComponent},
+  { path:'blogs',component:BlogsComponent},
+  { path:'contact',component:ContactComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
